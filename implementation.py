f057da48bbe407dda8a800beda0410eea7b419a4
iamrishap <rishap.sharma@iress.com>
2019-04-27 20:43:56 +1000

    Setting up the project files and basic structure.

---
+++ implementation.py
@@ -0,0 +1,13 @@
+
+# Question 1
+def viterbi_algorithm(State_File, Symbol_File, Query_File):
+    pass
+
+
+# Question 2
+def top_k_viterbi(State_File, Symbol_File, Query_File, k):
+    pass
+
+# Question 3
+def advanced_decoding(State_File, Symbol_File, Query_File):
+    pass
